daf39324a6835057ac8f84eae8b968ec921e565f
billscarp <billscarp@gmail.com>
2017-10-09 09:42:05 -0400

    connections

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'bill_s_7'
-});
+var connection;
+ 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bill_s_7'
+  });
+
+}
  
 connection.connect();
 
